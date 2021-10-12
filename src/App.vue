e320a8e996fdf5874dc14797137fc27ba74f23b2
apurbadh <erikduke182@gmail.com>
2021-10-12 21:00:52 +0545

    metamask and web3 initalized

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,30 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Navbar from "./components/Navbar.vue"
-</script>
+import { onMounted } from "vue";
+import Web3 from 'web3/dist/web3.min.js'
 
+onMounted(()=> {
+  if (window.web3)
+  {
+    window.web3 = new Web3(window.ethereum)
+    window.ethereum.enable()
+  }else{
+    alert("Please install MetaMask")
+  }
+})
+</script>
+<script>
+  export default{
+    data(){
+      return {
+        username : window.ethereum ? window.ethereum.selectedAddress : "Not Connected"
+      }
+    }
+  }
+</script>
 <template>
-  <Navbar username="0x04b489B90c2970C3e353147122ddA62A7421162a" />
+  <Navbar :username="username"/>
   <router-view></router-view>
 </template>
 
--- src/pages/Create.vue
+++ src/pages/Create.vue
@@ -1,3 +1,23 @@
+<script>
+	export default {
+		data(){
+			return {
+				user_input : ""
+			}
+		},
+		methods : {
+			createPost(){
+				console.log(this.user_input)
+			}
+		}
+	}
+</script>
+
 <template>
 	<h2 style="text-align: center; margin: 2rem;">Create Post</h2>
+
+	<div style="display: flex; justify-content: center;">
+		<input type="text" class="form-control" style="width: 40%;" placeholder="Write something" v-model="user_input">
+		<button class="btn btn-primary" @click="createPost">Create Post</button>
+	</div>
 </template>
